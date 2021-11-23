5496a494da13965b65a0394c210f4ec6725f04d6
Shane O'Hanlon <shaneaghada@gmail.com>
2021-11-23 08:57:40 +0000

    Set up register and login routes

--- app.js
+++ app.js
@@ -1,16 +1,75 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
+// ------------------------------------- DB SETUP
 
+mongoose.connect(process.env.MONGODB_URI);
+
+const userSchema = {
+    email: String,
+    password: String
+}
+
+const User = new mongoose.model('User', userSchema);
+
+// ------------------------------------- ROUTES
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.route('/register')
+    .get((req, res) => {
+        res.render('register');
+    })
+    .post((req, res) => {
+        const newUser = new User({
+            email: req.body.email,
+            password: req.body.password,
+        });
+
+        newUser.save((err) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.render('secrets');
+            }
+        });
+    });
+
+app.route('/login')
+    .get((req, res) => {
+        res.render('login');
+    })
+    .post((req, res) => {
+        const email = req.body.email;
+        const password = req.body.password;
+
+        User.findOne({ email: email }, (err, foundUser) => {
+            if (err) {
+                console.log(err);
+            } else {
+                if (foundUser) {
+                    if (foundUser.password === password) {
+                        res.render('secrets');
+                    }
+                }
+            }
+        })
+    });
 
 
 app.listen(3000, () => {
     console.log('Server started on port 3000.')
-})
+});
