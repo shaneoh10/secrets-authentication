3b36fdf2207e250a69ad0bd1f0c07913134e14c4
Shane O'Hanlon <shaneaghada@gmail.com>
2021-11-23 10:09:20 +0000

    Replace mongoose password encryption with md5 hashing

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 
 const app = express();
 
@@ -20,8 +20,6 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
-
 const User = new mongoose.model('User', userSchema);
 
 // ------------------------------------- ROUTES
@@ -37,7 +35,7 @@ app.route('/register')
     .post((req, res) => {
         const newUser = new User({
             email: req.body.email,
-            password: req.body.password,
+            password: md5(req.body.password)
         });
 
         newUser.save((err) => {
@@ -55,7 +53,7 @@ app.route('/login')
     })
     .post((req, res) => {
         const email = req.body.email;
-        const password = req.body.password;
+        const password = md5(req.body.password);
 
         User.findOne({ email: email }, (err, foundUser) => {
             if (err) {
